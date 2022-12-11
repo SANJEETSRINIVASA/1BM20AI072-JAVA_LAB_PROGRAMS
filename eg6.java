5cc640bd80c052c6b33a17a022b9ea6613492936
SANJEETSRINIVASA <101923622+SANJEETSRINIVASA@users.noreply.github.com>
2022-12-11 21:12:32 +0530

    Create eg6.java

---
+++ eg6.java
@@ -0,0 +1,9 @@
+import Employee.*;
+
+public class eg6 {
+    public static void main(String[] args) {
+        Emp e = new Emp("x", "0", "male", 1000);
+        e.calculate();
+        e.display();
+    }
+}
